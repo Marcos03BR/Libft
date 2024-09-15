29f9fe751e89d08893b3a705db09c32683945138
Marcos <marckos03br@gmail.com>
2024-09-15 18:34:42 +0200

    15/09/2024
    Ayer participé en el primer CTF de la liga de ciberseguridad por lo que no pude hacer mucho.
    
    Hoy he completado un par de códigos más de la parte adicional, especialmente los últimos que eran más livianos, para descansar un poco.

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbarranq <mbarranq@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/15 18:20:13 by mbarranq          #+#    #+#             */
+/*   Updated: 2024/09/15 18:28:02 by mbarranq         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	char	c;
+
+	if (n == -2147483648)
+	{
+		write(fd, "-2147483648", 11);
+	}
+	else if (n < 0)
+	{
+		write(fd, "-", 1);
+		ft_putnbr_fd(-n, fd);
+	}
+	else if (n >= 10)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
+	}
+	else
+	{
+		c = n + '0';
+		write(fd, &c, 1);
+	}
+}
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,30 @@
 /*   By: mbarranq <mbarranq@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 20:29:25 by mbarranq          #+#    #+#             */
-/*   Updated: 2024/09/13 21:09:28 by mbarranq         ###   ########.fr       */
+/*   Updated: 2024/09/15 17:52:52 by mbarranq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+char	*ft_strtrim(const char *s1, const char *set)
+{
+	size_t	start;
+	size_t	end;
+	char	*trimmed;
+
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	end = ft_strlen(s1);
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	while (end > start && ft_strchr(set, s1[end - 1]))
+		end--;
+	trimmed = (char *)malloc(sizeof(char) * (end - start + 1));
+	if (!trimmed)
+		return (NULL);
+	ft_memcpy(trimmed, &s1[start], end - start);
+	trimmed[end - start] = '\0';
+	return (trimmed);
+}
